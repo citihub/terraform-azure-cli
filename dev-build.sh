f3d42c49655ccb8b02faa1de3b07f132acc15bea
david sewell <david.sewell@citihub.com>
2021-02-24 11:28:03 +0000

    add post_push hook to tag docker image

--- dev-build.sh
+++ dev-build.sh
@@ -4,6 +4,9 @@ set -eo pipefail
 
 IMAGE_NAME="citihub/terraform-azure-cli:dev"
 
+LATEST_NAME="citihub/terraform-azure-cli:latest"
+
+
 # Lint Dockerfile
 echo "Linting Dockerfile..."
 docker run --rm -i hadolint/hadolint:v1.17.6-alpine < Dockerfile
@@ -17,6 +20,8 @@ if [ -n "$1" ] && [ -n "$2" ] && [ -n "$3" ]; then
   echo "Building release image ${IMAGE_NAME}..."
   docker image build --build-arg AZURE_CLI_VERSION="$1" --build-arg TERRAFORM_VERSION="$2" --build-arg DATABRICKS_CLI_VERSION="$3" -t $IMAGE_NAME .
   docker push ${IMAGE_NAME}
+  docker tag ${IMAGE_NAME} ${LATEST_NAME}
+  docker push ${LATEST_NAME}
 else
   echo "Building dev images with default parameters..."
   docker image build -f Dockerfile -t $IMAGE_NAME .
