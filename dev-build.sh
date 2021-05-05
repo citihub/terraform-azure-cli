6613bced9c0bc373fe92eb531eb05ac57bf02061
David Sewell <david.sewell@citihub.com>
2021-05-05 17:12:52 +0000

    update version numbers in tags

--- dev-build.sh
+++ dev-build.sh
@@ -15,7 +15,7 @@ echo "Lint Successful!"
 # Build dev image
 if [ -n "$1" ] && [ -n "$2" ] && [ -n "$3" ]; then
   echo "Building release image with ${TAG}..."
-  TAG="release-1.0_terraform-${1}_azcli-${2}_databricks-${3}"
+  TAG="release-1.1_azcli-${1}_terraform-${2}_databricks-${3}"
   IMAGE_NAME="citihub/terraform-azure-cli:${TAG}"
   echo "Building release image ${IMAGE_NAME}..."
   docker image build --build-arg AZURE_CLI_VERSION="$1" --build-arg TERRAFORM_VERSION="$2" --build-arg DATABRICKS_CLI_VERSION="$3" -t $IMAGE_NAME .
