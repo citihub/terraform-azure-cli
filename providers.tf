afd9edf6239597681e5a251b6bbf6caf14f077ce
David Sewell <david.sewell@citihub.com>
2021-06-29 21:26:44 +0000

    azure cli 2.21.0

--- providers.tf
+++ providers.tf
@@ -12,27 +12,27 @@ terraform {
     required_providers {
         azurerm = {
             source  = "hashicorp/azurerm"
-            version = ">= 2.64.0"
+            version = "= 2.64.0"
         }
         azuread = {
             source = "hashicorp/azuread"
-            version = ">= 1.4.0"
+            version = "= 1.4.0"
         }
         databricks = {
             source  = "databrickslabs/databricks"
-            version = ">= 0.3.1"
+            version = "= 0.3.1"
         }
         random = {
             source = "hashicorp/random"
-            version = ">= 3.1.0"
+            version = "= 3.1.0"
         }
         null = {
             source = "hashicorp/null"
-            version = ">= 3.1.0"
+            version = "= 3.1.0"
         }
         vault = {
             source = "hashicorp/vault"
-            version = ">= 2.21.0"
+            version = "= 2.21.0"
         }
     }
 }
